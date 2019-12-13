9a75d237943b08aba8d6d2bfde972d26b969061c
ljc88118 <2435066598@qq.com>
2019-12-13 10:41:19 +0800

    谷歌

--- config/auth.php
+++ config/auth.php
@@ -43,7 +43,7 @@ return [
 
         'api' => [
             'driver' => 'session',
-            'provider' => 'users',
+            'provider' => 'api',
             'hash' => false,
         ],
     ],
